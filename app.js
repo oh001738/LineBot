e42067e8a59d9e20a6cd05ec74433f0cbba568d5
Kaiser Tsai <sk.tsai@wuchipc.com>
2023-02-04 17:40:33 +0800

    add openai

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const line = require('@line/bot-sdk');
 const express = require('express');
-
+const line = require('@line/bot-sdk');
+const { Configuration, OpenAIApi } = require("openai");
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -27,16 +27,35 @@ app.post('/callback', line.middleware(config), (req, res) => {
       res.status(500).end();
     });
 });
-
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 // event handler
-function handleEvent(event) {
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: event.message.text ,
+	max_tokens: 200,
+  });
+
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
@@ -47,4 +66,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
-
