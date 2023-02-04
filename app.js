ea19f7210e4970985cbc0b1b98421ab5de3ff1a6
Kaiser Tsai <sk.tsai@wuchipc.com>
2023-02-04 18:25:33 +0800

    rollback to text-davinci-003

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ async function handleEvent(event) {
   }
 
   const completion = await openai.createCompletion({
-    model: "text-curie-001",
+    model: "text-davinci-003",
     prompt: event.message.text ,
     max_tokens: 500,
   });
