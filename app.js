f7c71ba9b310a8363f944ef2d6880d4293c298c2
Kaiser Tsai <sk.tsai@wuchipc.com>
2023-02-06 17:32:37 +0800

    add description

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+// Import the 'dotenv' package to access environment variables
+// stored in the '.env' file
 require('dotenv').config();
 
+// Import the '@line/bot-sdk' library for using the LINE Bot API
 const line = require('@line/bot-sdk');
+
+// Import the 'express' library for creating a web server
 const express = require('express');
 
-// create LINE SDK config from env variables
+// Create a configuration object for the LINE Bot API using environment variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
-// create LINE SDK client
+// Create a client for the LINE Bot API using the configuration object
 const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
+// Create an instance of the Express app
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
+// Register a handler for receiving webhook events from the LINE platform
 app.post('/callback', line.middleware(config), (req, res) => {
+  // Use 'Promise.all()' to handle all events in the webhook request body
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
@@ -28,22 +32,23 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
-// event handler
+// Event handler function to process incoming events
 function handleEvent(event) {
+  // If the event is not a message event or the message is not a text message,
+  // ignore the event
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  // create a echoing text message
+  // Create a text message to echo back the received message text
   const echo = { type: 'text', text: event.message.text };
 
-  // use reply API
+  // Use the reply API to send the echo message back to the user
   return client.replyMessage(event.replyToken, echo);
 }
 
-// listen on port
+// Start the Express app and listen on the specified port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
+  console.log(`Listening on port ${port}`);
 });
