4f042e6441532374b7c01698a2fe1516f9dd9b92
Kaiser Tsai <sk.tsai@wuchipc.com>
2023-02-06 12:00:21 +0800

    威力加強版2

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-require('dotenv').config();
-
 const express = require('express');
 const line = require('@line/bot-sdk');
-const { Configuration, OpenAIApi } = require("openai");
+const openai = require('openai');
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-// create LINE SDK config from env variables
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
-
-// create LINE SDK client
-const client = new line.Client(config);
-
-// create Express app
 const app = express();
 
-// register a webhook handler with middleware
+const port = process.env.PORT || 5000;
+
+const config = {
+  channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
+  channelSecret: 'YOUR_CHANNEL_SECRET'
+};
+
+openai.promisified = true;
+
+const client = new line.Client(config);
+
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
@@ -32,51 +25,31 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
-// event handler
 async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  if (event.message.text.startsWith("show me")) {
-    const completion = await openai.createImageCompletion({
-      model: "image-alpha-001",
-      prompt: event.message.text.substring(8),
-      n: 1,
+  const message = event.message.text;
+  
+  if (message === 'hey sk') {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: 'Hello! How can I help you today?'
     });
-
-    // create a image message
-    const image = {
+  } else if (message === 'show me') {
+    const response = await openai.ImageCompletion.create(model, prompt, n=1, temperature=0.5, maxTokens=1024);
+    const imageUrl = response.data[0].url;
+    return client.replyMessage(event.replyToken, {
       type: 'image',
-      originalContentUrl: completion.data.choices[0].image,
-      previewImageUrl: completion.data.choices[0].image,
-    };
-
-    // use reply API
-    return client.replyMessage(event.replyToken, image);
+      originalContentUrl: imageUrl,
+      previewImageUrl: imageUrl
+    });
+  } else {
+    return Promise.resolve(null);
   }
-
-  if (!event.message.text.startsWith("hey sk")) {
-    const response = { type: 'text', text: "請輸入 hey sk +問題 來找到我！" };
-    return client.replyMessage(event.replyToken, response);
-  }
-
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: event.message.text.substring(7),
-    max_tokens: 500,
-  });
-
-  // create a echoing text message
-  const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
 }
 
-// listen on port
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
