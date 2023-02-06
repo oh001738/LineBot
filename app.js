6c4921e932549876f0bf0efeb89e7348506937b8
Kaiser Tsai <sk.tsai@wuchipc.com>
2023-02-06 16:17:05 +0800

    優化

--- app.js
+++ app.js
@@ -1,58 +1,61 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
+const express = require("express");
+const line = require("@line/bot-sdk");
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 
-// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelSecret: process.env.CHANNEL_SECRET
 };
-
-// create LINE SDK client
 const client = new line.Client(config);
 
-// create Express app
 const app = express();
 
-app.post('/callback', line.middleware(config), async (req, res) => {
+app.post("/callback", line.middleware(config), async (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
+    .then(result => res.json(result))
+    .catch(err => {
       console.error(err);
       res.status(500).end();
     });
 });
 
 async function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
 
   if (event.message.text.startsWith("show me")) {
     const response = await openai.createImage({
-      prompt: event.message.text.substring(7),
+      prompt: event.message.text.substring(7)
     });
 
     const imageUrl = response.data.url;
-    const echo = { type: 'image', originalContentUrl: imageUrl, previewImageUrl: imageUrl };
+    const echo = {
+      type: "image",
+      originalContentUrl: imageUrl,
+      previewImageUrl: imageUrl
+    };
     return client.replyMessage(event.replyToken, echo);
   } else if (event.message.text.startsWith("hey sk")) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: event.message.text.substring(7) ,
-      max_tokens: 500,
+      prompt: event.message.text.substring(7),
+      max_tokens: 500
     });
 
-    const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
+    const echo = { type: "text", text: completion.data.choices[0].text.trim() };
     return client.replyMessage(event.replyToken, echo);
   } else {
-    const response = { type: 'text', text: "你是不是要找SK？請輸入 hey sk +問題 來找到我！或輸入 show me +描述 來獲得圖片。" };
+    const response = {
+      type: "text",
+      text: "你是不是要找SK？請輸入 hey sk +問題 來找到我！或輸入 show me +描述 來獲得圖片。"
+    };
     return client.replyMessage(event.replyToken, response);
   }
 }
