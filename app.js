440a4e43e96484e517e64d4bdb49a7af2426b1df
Kaiser Tsai <sk.tsai@wuchipc.com>
2023-02-07 13:47:54 +0800

    留下圖片產生器

--- app.js
+++ app.js
@@ -54,33 +54,24 @@ async function handleEvent(event) {
   }
 
   let echo;
-  if (event.message.text.startsWith("hey sk ")) {
-    const prompt = event.message.text.replace("hey sk ", "");
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt,
-      max_tokens: 200,
+  if (event.message.text.startsWith("show me ")) {
+    const prompt = event.message.text.replace("show me ", "");
+    const imageCompletion = await openai.createImage({
+      model: "image-alpha-001",
+      prompt: "A cute baby sea otter",
+      n: 1,
+      size: "256x256",
+      response_format: "url"
     });
-    echo = { type: 'text', text: completion.data.choices[0].text.trim() };
-  } else if (event.message.text.startsWith("show me ")) {
-   // const prompt = event.message.text.replace("show me ", "");
-    //const imageCompletion = await openai.createImage({
-      //model: "image-alpha-001",
-      //prompt: "A cute baby sea otter",
-      //n: 1,
-      //size: "256x256",
-      //response_format: "url"
-    //});
-    //const imageUrl = imageCompletion.data.images[0].url;
-    //console.log(imageCompletion.data.data[0].url);
-    imageUrl = "https://www.esunbank.com.tw/bank/-/media/New-ESUNBANK/Home/HugeBanner/FHC/drawesun_800_500.jpg?h=500&w=800&hash=3EB0A2D6452F6A5F9BEDB0D90381D2D0";
+    const imageUrl = imageCompletion.data.images[0].url;
+    console.log(imageCompletion.data.data[0].url);
     echo = {
       type: "image",
       originalContentUrl: imageUrl,
       previewImageUrl: imageUrl
     };
   } else {
-    // input message doesn't match "hey sk " or "show me "
+    // input message doesn't match "show me "
     return Promise.resolve(null);
   }
 
