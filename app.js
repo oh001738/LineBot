423422a4285f48db797b5fd2d919950258ca4228
Kaiser Tsai <sk.tsai@wuchipc.com>
2023-02-16 17:37:48 +0800

    minor fix

--- app.js
+++ app.js
@@ -55,9 +55,9 @@ async function handleEvent(event) {
   }
   let echo;
   const textCallSign = process.env.TEXT_CALL_SIGN;
-  const textCallSignLength = textCallSign.length;
+  const textCallSignLength = textCallSign.length + 1;
   const imageCallSign = process.env.IMAGE_CALL_SIGN;
-  const imageCallSignLength = imageCallSign.length;
+  const imageCallSignLength = imageCallSign.length + 1;
 
   const inputText = event.message.text.toLowerCase();
   if (inputText.startsWith(imageCallSign)){
