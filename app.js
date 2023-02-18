643eef9937a1f4373878d75004036e98443b026b
Kaiser Tsai <sk.tsai@wuchipc.com>
2023-02-18 14:45:52 +0800

    change model

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ async function handleEvent(event) {
   };
 } else if (inputText.startsWith(textCallSign)) {
   const completion = await openai.createCompletion({
-    model: "text-chat-davinci-002-20230126",
+    model: "text-chat-davinci-002",
     prompt: event.message.text.substring(imageCallSignLength),
     temperature: 0.6,
     max_tokens: 500,
